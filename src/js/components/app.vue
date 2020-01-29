c6756a76a08cc559420af11442ecb2c10f6a9837
Hay Kranen <huskyr@gmail.com>
2020-01-29 14:18:14 +0100

    Adding initial commit

---
+++ src/js/components/app.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="screen">
+        <h1>Hallo</h1>
+        <img src="/static/img/logo-wordmark.svg" alt="" />
+        <img src="/static/assets/1-sq.jpg" alt="" />
+    </div>
+</template>
