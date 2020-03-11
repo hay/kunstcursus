5846f070cdc4abc82b5b64940f12efaaac6f9df7
Hay Kranen <huskyr@gmail.com>
2020-03-11 11:43:59 +0100

    Intro seems to work

--- src/js/components/screen-intro.vue
+++ src/js/components/screen-intro.vue
@@ -1,21 +1,98 @@
 <template>
     <div class="screen intro">
-        <div
-            ref="canvas"
-            class="intro__canvas"></div>
+        <div v-if="loading"
+             class="intro__loading">
+            {{$msg('loading_intro')}}
+        </div>
+
+        <div v-if="!loading"
+             v-bind:style="progressStyle"
+             class="intro__progress"></div>
+
+        <div ref="canvas"
+             class="intro__canvas"></div>
+
+        <el-button
+            class="intro__skip"
+            v-on:click="close"
+            msg="skip_intro"></el-button>
     </div>
 </template>
 
 <script>
     import lottie from 'lottie-web';
 
+    let animation;
+
     export default {
+        data() {
+            return {
+                done : 0,
+
+                loading : true,
+
+                progressStyle : {
+                    width : '100%'
+                },
+
+                voiceover : false
+            };
+        },
+
+        methods : {
+            close() {
+                animation.destroy();
+                this.$store.commit('screen', 'profile');
+            },
+
+            setupAnimation() {
+                animation = lottie.loadAnimation({
+                    container : this.$refs.canvas,
+                    path : 'intro/data.json',
+                    autoplay : false
+                });
+
+                animation.addEventListener('loaded_images', () => {
+                    this.loading = false;
+                    animation.play();
+                })
+
+                animation.addEventListener('enterFrame', (e) => {
+                    const pct = e.currentTime / e.totalTime;
+                    this.progressStyle.width = ((1 - pct) * 100) + '%';
+
+                    if (pct > 0.05 && !this.voiceover) {
+                        this.startVoiceover();
+                    }
+                });
+
+                animation.addEventListener('complete', () => {
+                    this.done = this.done + 1;
+                })
+            },
+
+            startVoiceover() {
+                this.voiceover = true;
+                const sound = this.$sounds.play('intro_01');
+
+                sound.player.addEventListener('ended', () => {
+                    this.done = this.done + 1;
+                });
+            }
+        },
+
         mounted() {
-            lottie.loadAnimation({
-                container : this.$refs.canvas,
-                path : 'intro/data.json',
-                autoplay : true
-            });
+            this.setupAnimation();
+        },
+
+        watch : {
+            done() {
+                // This is a bit of a kludge, but make sure both voiceover
+                // *and* animation are finished
+                if (this.done >= 2) {
+                    this.close();
+                }
+            }
         }
     }
 </script>
